c7831cb4b88a3aac9ed4e1c6c1d9b50e81cf0aee
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2023-07-31 22:14:06 -0300

    quase

--- paginadecompras/index.php
+++ paginadecompras/index.php
@@ -1,126 +1,95 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Papelaria Novo Mundo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-
-  <nav class="navbar navbar-expand-lg cabecalho">
-    <div class="container-fluid">
-      <a class="navbar-brand text-light" href="../pagina/index.php"><img src="../img/icone.png" class="mb-1" alt="Icone" width="50">Papelaria Novo Mundo</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item  ">
-            <h5><a class="nav-link" aria-current="page" href="../pagina/index.php">Home</a></h5>
-          </li>
-          <li class="nav-item">
-            <h5> <a class="nav-link" href="#">Destaques</a></h5>
-          </li>
-          <li class="nav-item">
-            <h5><a class="nav-link" href="#">Produtos</a></h5>
-          </li>
-        </ul>
-
-        <a href="../paginalogin/login.php">
-          <button class="btn btn-danger  ms-2" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Entrar</button>
-        </a>
-
-        <a href="#">
-          <button class="btn btn-danger  ms-2" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Registrar</button>
-        </a>
+<?php
+include "../includes/cabecalho.php"
+?>
 
 
-        <form class="d-flex ms-5" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-danger bg-danger-subtle " type="submit"><img class="mb-1" src="../img/lupa.png" width="15" alt=""></button>
-        </form>
 
-
-      </div>
-    </div>
-  </nav>
-
-<br>
 <div class="row" style="height: 600px;">
   <div class="col-md-1">
     <!-- Conteúdo da primeira coluna -->
   </div>
-  <div class="col-md-4 border border-black">
+  <div class="col-md-4">
     <!-- Conteúdo da segunda coluna, incluindo a imagem -->
     <img src="../img/icone.png" class="img-fluid" alt="...">
   </div>
-  <div class="text-center col-md-6 col-xs-x border border-black">
+  <div class="text-center col-md-6 col-xs-x">
     <!-- Conteúdo vazio para ocupar espaço na mesma linha -->
     <br>
     <p class="text-center fs-4">Lapis Da Farber Castel</p>
     <p class=" badge bg-danger text-wrap fs-6" style="width: 10rem;">Marca: Faber Castel</p>
     <br>
 
-<div class="estrela">
+    <div class="estrela">
 
-    <a href="javascript:void(0)" onclick="Avaliar(1)">
-<img class="estreladesligada" src="../img/star (2).png" id="s1"></a>
+      <a href="javascript:void(0)" onclick="Avaliar(1)">
+        <img class="estreladesligada" src="../img/star (2).png" id="s1"></a>
 
-<a href="javascript:void(0)" onclick="Avaliar(2)">
-<img class="estreladesligada" src="../img/star (2).png" id="s2"></a>
+      <a href="javascript:void(0)" onclick="Avaliar(2)">
+        <img class="estreladesligada" src="../img/star (2).png" id="s2"></a>
 
-<a href="javascript:void(0)" onclick="Avaliar(3)">
-<img class="estreladesligada" src="../img/star (2).png" id="s3"></a>
+      <a href="javascript:void(0)" onclick="Avaliar(3)">
+        <img class="estreladesligada" src="../img/star (2).png" id="s3"></a>
 
-<a href="javascript:void(0)" onclick="Avaliar(4)">
-<img class="estreladesligada" src="../img/star (2).png" id="s4"></a>
+      <a href="javascript:void(0)" onclick="Avaliar(4)">
+        <img class="estreladesligada" src="../img/star (2).png" id="s4"></a>
 
-<a href="javascript:void(0)" onclick="Avaliar(5)">
-<img class="estreladesligada" src="../img/star (2).png" id="s5"></a>
-<p id="rating"></p>
-</div>
+      <a href="javascript:void(0)" onclick="Avaliar(5)">
+        <img class="estreladesligada" src="../img/star (2).png" id="s5"></a>
 
-<p class="text-center fs-10">De Sua Valiação</p>
+    </div>
 
-<p class="text-center fs-4">R$ 8,40</p>
+    <p class="text-center fs-10">De Sua Valiação</p>
 
-<br>
+    <p class="text-center fs-4">R$ 8,40</p>
 
-<a class="fancy" href="#">
-  <span class="top-key"></span>
-  <span class="text">Comprar</span>
-  <span class="bottom-key-1"></span>
-  <span class="bottom-key-2"></span>
-</a>
+    <br>
+
+    <a class="fancy" href="#">
+      <span class="top-key"></span>
+      <span class="text">Comprar</span>
+      <span class="bottom-key-1"></span>
+      <span class="bottom-key-2"></span>
+    </a>
+
+    <br>
+    <br>
+    <div class="container mt-5 ">
+        <div class="input-group mb-3">
+            <button id="btn-subtract" class="btn btn-primary" type="button">-</button>
+            <input type="text" id="input-number" class="form-control text-center" value="0" aria-label="Number">
+            <button id="btn-add" class="btn btn-primary" type="button">+</button>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="input-group input-group-sl w-50 mb-5">
+        <span class="input-group-text">$</span>
+        <span class="input-group-text">0.00</span>
+        <input type="text" class="form-control " aria-label="Dollar amount (with dot and two decimal places)">
+      </div>
+    </div>
 
   </div>
 </div>
 
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h3>Comentários</h3>
 
-
-
-
-
-
-
-
-<br>
-<br>
-
-
-
-<footer class="bg-gradient rodape">
-<p style="text-align:center; color: dark;">
-&copy; Todos os direitos reservados. Criado pelos alunos do Senac - Geovana, Guilherme, Henrique, Pedro Santos
-
-</p>
-</footer>
+      <div class="form-floating w-50 ms-3">
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+        <label for="floatingTextarea2">Comentario</label>
+      </div>
+      <button type="button" class="btn btn-outline-danger ms-3">Enviar</button>
+    </div>
+  </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <script src="script.js"></script>
+
+
+
+
 </body>
+
 </html>
--- paginadecompras/script.js
+++ paginadecompras/script.js
@@ -9,96 +9,110 @@ function Avaliar(estrela) {
     var s3 = document.getElementById("s3").src;
     var s4 = document.getElementById("s4").src;
     var s5 = document.getElementById("s5").src;
-    var avaliacao = 0;
+
    
    if (estrela == 5){ 
-    if (s5 == url + "../img/star (2).png") {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (3).png";
-    document.getElementById("s3").src = "../img/star (3).png";
-    document.getElementById("s4").src = "../img/star (3).png";
-    document.getElementById("s5").src = "../img/star (3).png";
-    avaliacao = 5;
+    if (s5 == url + "../img/staroff.png") {
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/star.png";
+    document.getElementById("s4").src = "../img/star.png";
+    document.getElementById("s5").src = "../img/star.png";
+    
     } else {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (3).png";
-    document.getElementById("s3").src = "../img/star (3).png";
-    document.getElementById("s4").src = "../img/star (3).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 4;
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/star.png";
+    document.getElementById("s4").src = "../img/star.png";
+    document.getElementById("s5").src = "../img/star.png";
+    
    }}
     
     //ESTRELA 4
    if (estrela == 4){ 
-    if (s4 == url + "../img/star (2).png") {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (3).png";
-    document.getElementById("s3").src = "../img/star (3).png";
-    document.getElementById("s4").src = "../img/star (3).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 4;
+    if (s4 == url + "../img/staroff.png") {
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/star.png";
+    document.getElementById("s4").src = "../img/star.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
     } else {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (3).png";
-    document.getElementById("s3").src = "../img/star (3).png";
-    document.getElementById("s4").src = "../img/star (2).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 3;
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/star.png";
+    document.getElementById("s4").src = "../img/star.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
    }}
    
    //ESTRELA 3
    if (estrela == 3){ 
-    if (s3 == url + "../img/star (2).png") {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (3).png";
-    document.getElementById("s3").src = "../img/star (3).png";
-    document.getElementById("s4").src = "../img/star (2).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 3;
+    if (s3 == url + "../img/staroff.png") {
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/star.png";
+    document.getElementById("s4").src = "../img/star.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
     } else {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (3).png";
-    document.getElementById("s3").src = "../img/star (2).png";
-    document.getElementById("s4").src = "../img/star (2).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 2;
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/star.png";
+    document.getElementById("s4").src = "../img/staroff.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
    }}
     
    //ESTRELA 2
    if (estrela == 2){ 
-    if (s2 == url + "../img/star (2).png") {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (3).png";
-    document.getElementById("s3").src = "../img/star (2).png";
-    document.getElementById("s4").src = "../img/star (2).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 2;
+    if (s2 == url + "../img/staroff.png") {
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/staroff.png";
+    document.getElementById("s4").src = "../img/staroff.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
     } else {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (2).png";
-    document.getElementById("s3").src = "../img/star (2).png";
-    document.getElementById("s4").src = "../img/star (2).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 1;
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/star.png";
+    document.getElementById("s3").src = "../img/staroff.png";
+    document.getElementById("s4").src = "../img/staroff.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
    }}
     
     //ESTRELA 1
    if (estrela == 1){ 
-    if (s1 == url + "../img/star (2).png") {
-    document.getElementById("s1").src = "../img/star (3).png";
-    document.getElementById("s2").src = "../img/star (2).png";
-    document.getElementById("s3").src = "../img/star (2).png";
-    document.getElementById("s4").src = "../img/star (2).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 1;
+    if (s1 == url + "../img/star.png") {
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/staroff.png";
+    document.getElementById("s3").src = "../img/staroff.png";
+    document.getElementById("s4").src = "../img/staroff.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
     } else {
-    document.getElementById("s1").src = "../img/star (2).png";
-    document.getElementById("s2").src = "../img/star (2).png";
-    document.getElementById("s3").src = "../img/star (2).png";
-    document.getElementById("s4").src = "../img/star (2).png";
-    document.getElementById("s5").src = "../img/star (2).png";
-    avaliacao = 0;
+    document.getElementById("s1").src = "../img/star.png";
+    document.getElementById("s2").src = "../img/staroff.png";
+    document.getElementById("s3").src = "../img/staroff.png";
+    document.getElementById("s4").src = "../img/staroff.png";
+    document.getElementById("s5").src = "../img/staroff.png";
+    
    }}
     
     document.getElementById('rating').innerHTML = avaliacao;
 }
+// Obtém o elemento do input
+const inputElement = document.getElementById("input-number");
+
+// Botão para adicionar 1 ao número
+document.getElementById("btn-add").addEventListener("click", function() {
+  let currentNumber = parseFloat(inputElement.value);
+  inputElement.value = (currentNumber + 1).toString();
+});
+
+// Botão para subtrair 1 do número
+document.getElementById("btn-subtract").addEventListener("click", function() {
+  let currentNumber = parseFloat(inputElement.value);
+  inputElement.value = (currentNumber - 1).toString();
+});
